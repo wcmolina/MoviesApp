bca820100a6e068ab4796773daba54bd44665e01
Wilmer Carranza <wcarranzam91@gmail.com>
2019-06-11 16:49:48 -0600

    Added Video and VideoList data classes. Removed default text from text views

---
+++ app/src/main/java/android/example/moviesapp/models/Video.kt
@@ -0,0 +1,9 @@
+package android.example.moviesapp.models
+
+data class Video
+(
+        val size: Int,
+        val name: String,
+        val key: String,
+        val type: String
+)
---
+++ app/src/main/java/android/example/moviesapp/models/VideoList.kt
@@ -0,0 +1,7 @@
+package android.example.moviesapp.models
+
+data class VideoList
+(
+        val id: Int,
+        val results: ArrayList<Video>
+)
