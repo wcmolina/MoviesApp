c7dfa59acb8185a4d30f9629107e4a1807d24c56
Wilmer Carranza <wcarranzam91@gmail.com>
2019-06-13 13:47:15 -0600

    Sticky immersive mode when playing video

--- app/src/main/java/android/example/moviesapp/YoutubePlayerActivity.java
+++ app/src/main/java/android/example/moviesapp/YoutubePlayerActivity.java
@@ -65,7 +65,7 @@ public class YoutubePlayerActivity extends AppCompatActivity {
 
     public void immersiveMode() {
         getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
